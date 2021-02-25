7801f8a13cda52103e763169f5a18bfd33e80c0a
Stephen McNearney <stephen@hownow.media>
2021-02-25 12:46:34 -0500

    Inital Commit. DigitalCrafts Week 11 Day 4 - React App - Memory Game, Part 1

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import MemoryCard from './components/MemoryCard.jsx'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Memory Game</h1>
+        <h2>Match cards to win</h2>
+      </header>
+      <div><MemoryCard /><MemoryCard /><MemoryCard /><MemoryCard /></div>
+      <div><MemoryCard /><MemoryCard /><MemoryCard /><MemoryCard /></div>
+      <div><MemoryCard /><MemoryCard /><MemoryCard /><MemoryCard /></div>
+      <div><MemoryCard /><MemoryCard /><MemoryCard /><MemoryCard /></div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/MemoryCard.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './MemoryCard.css';
+
+class MemoryCard extends Component {
+    render() {
+        return (
+            <div class="MemoryCard">
+                <img src="https://www.digitalcrafts.com/img/digitalcrafts-logo-white-y.png" />
+            </div>
+        );
+    }
+}
+
+
+export default MemoryCard;
